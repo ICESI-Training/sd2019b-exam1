77e8e2687b2e4e529deef481015497a64e597e99
zapata <you@example.com>
2019-09-30 21:32:49 -0500

    psql commands executing from sls :')

--- saltstack/salt/database/create_user.sh
+++ saltstack/salt/database/create_user.sh
@@ -1,8 +1,1 @@
-sudo su - postgres
-psql
-CREATE USER postgres_user WITH PASSWORD 'password';
-CREATE DATABASE distribuidos OWNER postgres_user;
-\q
-exit
-sudo su - postgres_user
-psql distribuidos
+sudo -u postgres -i -H sh -c "psql -c '\h'"
