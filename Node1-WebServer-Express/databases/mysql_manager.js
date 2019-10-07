5019b5d4b0a6ff4a374ce8142b9b68697f22708f
WMC <ingmartic@gmail.com>
2019-10-07 01:07:44 -0500

    POST a new user into database and GET all the users created in the database

--- Node1-WebServer-Express/databases/mysql_manager.js
+++ Node1-WebServer-Express/databases/mysql_manager.js
@@ -1,1 +1,21 @@
 'use strict'
+
+// References: https://www.npmjs.com/package/mysql
+
+// Define the environmental variables (see .sample-env file and edit into .env)
+var environmentalConfig = require('dotenv');
+environmentalConfig.config();
+
+// Database connector configuration:
+// References: https://stackoverflow.com/questions/44946270/er-not-supported-auth-mode-mysql-server
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: process.env.DATABASE_HOSTNAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_SCHEMA,
+    insecureAuth: true,
+    port: process.env.DATABASE_PORT
+});
+
+module.exports = connection;
